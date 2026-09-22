86e0c7c3aad2d9dd9ca48b67b6dc5b49b884dbde
Takeshi Kovacs <miotkdawid@gmail.com>
2026-09-22 21:50:58 +0200

    test: add automated scripts to TC004 and TC005

--- tests/ui-js/LoginPagePractice/negativeFormValidation.spec.js
+++ tests/ui-js/LoginPagePractice/negativeFormValidation.spec.js
@@ -4,7 +4,26 @@ const { FormPage } = require("../../../src/pages/LoginPagePractice/FormPage");
 test("Should display validation error when Name field is shorter than 2 characters", async({page}) =>{
     const formPage = new FormPage(page);
     await formPage.goToPage();
-    await formPage.fillName("T");
-    await expect(formPage.validationErrorLocator).toBeVisible();
+    await formPage.fillNameAndBlur("T");
+    await expect(formPage.nameLengthErrLocator).toBeVisible();
     await expect(formPage.nameInputLocator).toHaveClass(/ng-invalid/);
+})
+
+test("Should display validation error when name field is empty", async({page}) =>{
+    const formPage = new FormPage(page);
+//najezdza i bluruje field name 
+await formPage.goToPage();
+await formPage.nameInputLocator.focus();
+await formPage.nameInputLocator.blur();
+await expect(formPage.nameInputLocator).toHaveClass(/ng-invalid/);
+await expect(formPage.nameRequiredErrLoc).toBeVisible();
+
+})
+
+test("Should display validation error when email field is empty", async({page}) =>{
+    //najezdza i bluruje field email
+await formPage.emailInputLocator.focus();
+await formPage.emailInputLocator.blur();
+await expect(formPage.emailInputLocator).toHaveClass(/ng-invalid/);
+await expect(formPage.emailRequiredErrLoc).toBeVisible();
 })
